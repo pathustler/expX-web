a0ac261e4d8b2fc3a164739dcd569d2eeb480667
pathustler <patlow.harry@gmail.com>
2023-06-14 03:18:11 +0530

    Attempt to fix redirect error

--- expx-web/components/lessonspage/PageContent.tsx
+++ expx-web/components/lessonspage/PageContent.tsx
@@ -5,7 +5,7 @@ const PageContent = () => {
   return (
     <div className="flex items-center justify-center h-full">
       <div className="bg-gray-100 rounded-xl px-5 pt-5 w-4/6 max-h-80-percent overflow-y-auto custom-scrollbar">
-        <div className="p-4">
+        <div>
           <p className="text-gray-700 p-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis euismod felis, id dapibus lectus pretium vel. Vestibulum at nisi nec lorem semper hendrerit. Proin a mi vel ipsum tempor aliquet. Suspendisse eu orci non massa lobortis tempus. Aenean condimentum mauris et tellus sagittis posuere. Maecenas eget sapien urna. Curabitur nec commodo massa. Mauris pharetra lacus ac tortor accumsan dignissim.
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis euismod felis, id dapibus lectus pretium vel. Vestibulum at nisi nec lorem semper hendrerit. Proin a mi vel ipsum tempor aliquet. Suspendisse eu orci non massa lobortis tempus. Aenean condimentum mauris et tellus sagittis posuere. Maecenas eget sapien urna. Curabitur nec commodo massa. Mauris pharetra lacus ac tortor accumsan dignissim.
